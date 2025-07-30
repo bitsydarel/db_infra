0c53d55a035d405b9c32d05c78225b535c68a740
darelbitsy <bitsydarel@gmail.com>
2025-07-30 19:42:29 +0100

    Draft Release 0.5.1

--- lib/src/build_executor/flutter_ios_build_executor.dart
+++ lib/src/build_executor/flutter_ios_build_executor.dart
@@ -231,6 +231,7 @@ class FlutterIosBuildExecutor extends BuildExecutor {
           'build',
           'ipa',
           '--release',
+          '--verbose',
           '--no-codesign',
           if (dartDefines != null) ...dartDefines,
         ],
